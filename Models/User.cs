453fb0c05ce55a16bd21b9c0eee7160c565a8ec1
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2026-03-29 20:49:54 +0700

    add user.cs dan userrole.cs

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicineSystemAPI.Models;
+
+public partial class User
+{
+    public int Userid { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public DateTime CreatedDate { get; set; }
+
+    public byte? IsActive { get; set; }
+
+    public byte? RoleId { get; set; }
+
+    public int? DoctorId { get; set; }
+
+    public int? PharmacistId { get; set; }
+
+    public string Password { get; set; } = null!;
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicineSystemAPI.Models;
+
+public partial class UserRole
+{
+    public byte Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
