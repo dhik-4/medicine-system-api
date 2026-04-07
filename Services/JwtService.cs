23817f67a7744aa6b39f574e23cbb059336f480a
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2026-04-07 22:01:37 +0700

    add jwtservice.cs

---
+++ Services/JwtService.cs
@@ -0,0 +1,53 @@
+﻿using MedicineSystemAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MedicineSystemAPI.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _config;
+
+        public JwtService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Dictionary<string, string> GenerateAccessToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim("RoleId", user.RoleId.ToString()),
+                new Claim("UserId", user.Userid.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"])
+            );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            DateTime created = DateTime.Now;
+            DateTime expire = created.AddSeconds(
+                    Convert.ToDouble(_config["Jwt:AccessTokenSeconds"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: expire,
+                signingCredentials: creds
+            );
+
+            string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new Dictionary<string, string> {
+                { "token", jwtToken },
+                {"created", created.ToString() },
+                {"expire", expire.ToString() }
+            };
+        }
+    }
+}
